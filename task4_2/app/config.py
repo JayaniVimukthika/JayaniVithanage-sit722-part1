838cafdbcf340b27c2b2a66d00918df15d4190cd
JayaniVimukthika <144791020+JayaniVimukthika@users.noreply.github.com>
2024-08-24 18:15:18 +1000

    Update config.py

--- task4_2/app/config.py
+++ task4_2/app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://postgresql_aw66_user:MwxzGjRp2zudQKkrx9DdPkZJn6R3zS1t@dpg-cr4of5l2ng1s73e4ilpg-a.oregon-postgres.render.com/postgresql_aw66")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
